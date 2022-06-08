36101d759a660070f47d9b54a34e3b6c5f7e88c1
benny1020 <benny1020@naver.com>
2022-06-08 15:32:16 +0900

    ADD checkTileRelativeTest for Tile.java

--- tests/src/test/java/TileTest.java
+++ tests/src/test/java/TileTest.java
@@ -48,5 +48,28 @@ class TileTest {
         Assertions.assertTrue(tile.array() == 5);
     }
 
+    /*
+     * Purpose: check relative between tile or point
+     * Input: tile or point(x,y)
+     * Expected: return SUCCESS
+     */
+    @Test
+    void checkTileRelativeTest() {
+        Tile tile = new Tile(5,5);
+        Tile tile1 = new Tile(4,5);
+        Tile tile2 = new Tile(5,4);
+        Tile tile3 = new Tile(6,5);
+        Tile tile4 = new Tile(5,6);
+        Assertions.assertTrue(tile.relativeTo(tile1) == 2);
+        Assertions.assertTrue(tile.relativeTo(tile2) == 3);
+        Assertions.assertTrue(tile.relativeTo(tile3) == 0);
+        Assertions.assertTrue(tile.relativeTo(tile4) == 1);
+
+        Assertions.assertTrue(tile.relativeTo(4,5) == 2);
+        Assertions.assertTrue(tile.relativeTo(5,4) == 3);
+        Assertions.assertTrue(tile.relativeTo(6,5) == 0);
+        Assertions.assertTrue(tile.relativeTo(5,6) == 1);
+    }
+
 
 }
