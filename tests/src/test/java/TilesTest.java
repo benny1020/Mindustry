ee5a93eb25fc3057c2be4e265a8da6bc45d18207
benny1020 <benny1020@naver.com>
2022-06-08 16:02:18 +0900

    ADD checkIteratorTile for Tiles.java

--- tests/src/test/java/TilesTest.java
+++ tests/src/test/java/TilesTest.java
@@ -1,7 +1,12 @@
+import mindustry.world.Tile;
 import mindustry.world.Tiles;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+
+import static mindustry.Vars.world;
 
 class TilesTest {
 
@@ -30,6 +35,30 @@ class TilesTest {
         Assertions.assertTrue(tiles.in(5,5) == false);
 
 
+    }
+
+    /*
+     * Purpose: check fill() and Iterator
+     * Input: Tiles with fill()
+     * Expected: return SUCCESS
+     */
+
+    @Test
+    void checkIteratorTile() {
+        int width = 5;
+        int height = 5;
+        Tiles tiles = new Tiles(width,height);
+        tiles.fill();
+
+        Iterator<Tile> it = tiles.iterator();
+
+        int i = 0;
+
+        while(it.hasNext()){
+
+            Assertions.assertTrue(it.next().array() == (i%width) + (i/width) * world.tiles.width   );
+            i += 1;
+        }
     }
 
 
